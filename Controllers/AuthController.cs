cde6ec8278f0df318eea48fbb2595e6f9c4bf475
thayaraujo <thaynara_araujo1@yahoo.com.br>
2025-01-23 11:37:46 -0300

    updating configurations

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -10,33 +10,33 @@ public class AuthController : ControllerBase
 {
     private readonly string _key = "secret-key"; // Substitua pela chave
 
-    [HttpPost("login")]
-    public IActionResult Login([FromBody] UserLogin user)
-    {
+    //[HttpPost("login")]
+    //public IActionResult Login([FromBody] UserLogin user)
+    //{
 
-        if (user.Username != "admin" || user.Password != "password")
-        {
-            return Unauthorized();
-        }
+    //    if (user.Username != "admin" || user.Password != "password")
+    //    {
+    //        return Unauthorized();
+    //    }
 
-        var tokenHandler = new JwtSecurityTokenHandler();
-        var key = Encoding.ASCII.GetBytes(_key);
+    //    var tokenHandler = new JwtSecurityTokenHandler();
+    //    var key = Encoding.ASCII.GetBytes(_key);
 
-        var tokenDescriptor = new SecurityTokenDescriptor
-        {
-            Subject = new ClaimsIdentity(new[]
-            {
-                new Claim(ClaimTypes.Name, user.Username)
-            }),
-            Expires = DateTime.UtcNow.AddHours(1),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-        };
+    //    var tokenDescriptor = new SecurityTokenDescriptor
+    //    {
+    //        Subject = new ClaimsIdentity(new[]
+    //        {
+    //            new Claim(ClaimTypes.Name, user.Username)
+    //        }),
+    //        Expires = DateTime.UtcNow.AddHours(1),
+    //        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+    //    };
 
-        var token = tokenHandler.CreateToken(tokenDescriptor);
-        var tokenString = tokenHandler.WriteToken(token);
+    //    var token = tokenHandler.CreateToken(tokenDescriptor);
+    //    var tokenString = tokenHandler.WriteToken(token);
 
-        return Ok(new { Token = tokenString });
-    }
+    //    return Ok(new { Token = tokenString });
+    //}
 }
 
 public class UserLogin
--- ReadMe.md
+++ ReadMe.md
@@ -1,10 +1,20 @@
-#Projeto - API para consulta à API pública do GitHub
+# Projeto - API para consulta à API pública do GitHub
 API desenvolvida em .NET 9 utilizando C#, com o intuito de realizar consulta à API do GitHub (https://docs.github.com/rest)
 
-###Diretório API
+### Diretório API
 Contém o projeto .NET
 
 ### Diretório Flow
 Contém o fluxo de um chatbot em JSON. O chatbot foi desenvolvido utilizando a plataforma Blip. 
 
+### Link de acesso à API:
+https://consultagithub.azurewebsites.net/swagger/index.html
+
+
+
+
+----
+
+### Instruções para rodar o projeto
+Após fazer o clone, será necessário criar uma imagem no docker para rodar o projeto
 
